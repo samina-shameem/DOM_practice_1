593cf6f5032b3b991a5a9ac206968d3993bb5885
samina-shameem <samina.shameem.clt@gmail.com>
2024-12-11 13:30:54 +0100

    change js codes and html tags.

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const title = document.getElementById("title");
+    const button = document.getElementById("changeText");
+
+    button.addEventListener("click", () => {
+        title.textContent = "You clicked the button";
+        title.style.color = "blue";
+        console.log("Button was clicked");
+    });
+});
